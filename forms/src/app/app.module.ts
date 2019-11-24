5e6e5f7d4b3579393c585fe7a37020b69ed97766
Ricardo Santos <ri.santos@hotmail.com>
2019-11-24 20:53:04 -0300

    delete-files

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormModule } from './template-form/template-form.module';
-import { DataFormModule } from './data-form/data-form.module';
 import { ClientFormModule } from './client/client.module';
 import { OrderFormModule } from './order/order.module';
 import { SharedModule } from './shared/shared/shared.module';
@@ -20,9 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    TemplateFormModule,
     ReactiveFormsModule,
-    DataFormModule,
     ClientFormModule,
     SharedModule,
     OrderFormModule,
